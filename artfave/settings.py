d9fe83b66119b3b45439d86456501d65111d9653
Jessica Kincaid <4j.kincaid@gmail.com>
2025-02-12 11:16:29 -0600

    set debug to false

--- artfave/settings.py
+++ artfave/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure--@$vno77u#5$bypuoo6riw*shybi82o30!)z!)^^le-3*f2$iw"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["127.0.0.1", "jkincaid.pythonanywhere.com"]
 
